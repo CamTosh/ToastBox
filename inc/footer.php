384c3f7ea3f5b0c700a0700b6b3d04ac7a981d34
Tosh Camille <tochecamille@gmail.com>
2016-02-24 15:16:50 +0100

    fix github link

--- inc/footer.php
+++ inc/footer.php
@@ -3,5 +3,5 @@
     </div>	
     </main>
   </body>
-  <footer>Auteur : <a href="https://github.com/CamTosh/Toast-Box/" target="_blank"><i>Tosh Camille</i></a></footer>
+  <footer>Auteur : <a href="https://github.com/CamTosh/ToastBox" target="_blank"><i>Tosh Camille</i></a></footer>
 </html>
