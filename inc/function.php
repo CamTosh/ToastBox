f6993e37ed1f4f322710fd9740cc1eea07eaa6b3
gnieark <gnieark@free.fr>
2016-05-15 18:51:14 +0200

    glob

--- inc/function.php
+++ inc/function.php
@@ -7,33 +7,31 @@ require 'imdb/imdb.class.php';
  * Scan folder
  *
  */
-
+if ( ! function_exists('glob_recursive'))
+{
+    // Does not support flag GLOB_BRACE
+   
+    function glob_recursive($pattern, $flags = 0){
+        $files = glob($pattern, $flags);
+       
+        foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir)
+        {
+            $files = array_merge($files, glob_recursive($dir.'/'.basename($pattern), $flags));
+        }
+       
+        return $files;
+    }
+}
 function scan($path, $video_type) {
 
-	echo '<div class="item-grid">';
-
-	foreach($video_type as $ext) {
-		
-	   foreach(glob(''.$path.'/*.'.$ext.'') as $video) {
-
-	   		$file_name = basename($video);
-			$slash = 1;
-		    vignette($file_name, $slash);    
-	    }
-	    foreach(glob(''.$path.'/*/*.'.$ext.'') as $video) {
-
-	   		$file_name = basename($video);
-		    $slash = 2;
-		    vignette($file_name, $slash);    
-	    }
-	    foreach(glob(''.$path.'/*/*/*.'.$ext.'') as $video) {
-
-	   		$file_name = basename($video);
-		    $slash = 3;
-		    vignette($file_name, $slash);    
-	    }
-	}
-	echo '</div>';
+    echo '<div class="item-grid">';
+    
+    foreach(glob_recursive(''.$path.'/*.'."{".implode(',',$video_type).'}') as $video) {
+        $file_name = basename($video);
+	$slash = 1;
+        vignette($file_name, $slash);    
+    }	
+    echo '</div>';
 }
 
 /**
